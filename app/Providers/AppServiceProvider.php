745a40e8d05f249c4da57af69359a123c4bebdbc
vprikalab <vprikalab@gmail.com>
2020-10-17 21:29:07 +0300

    Add register post service

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Services\Contracts\PostServiceInterface;
+use App\Services\PostService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app
+            ->singleton(PostServiceInterface::class, function () {
+                return new PostService();
+            });
     }
 
     /**
